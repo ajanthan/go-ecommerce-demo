a9d022d95e66352296d5a59c6362dbeeda52f45a
Ajanthan Balachandran <balaajanthan@gmail.com>
2019-04-14 01:45:39 -0700

    closing queries

--- CartService/pkg/data/cart_repository.go
+++ CartService/pkg/data/cart_repository.go
@@ -21,10 +21,11 @@ func (c *CartRepository) InitRepository(connectionStr string) {
 	c.Db = db
 }
 func (c *CartRepository) AddItemToCart(userID string, item model.Item) {
-	_, queryErr := c.Db.Query("insert into cart (userID,productID,quantity) values(?,?,?)", userID, item.ProductID, item.Quantity)
+	rows, queryErr := c.Db.Query("insert into cart (userID,productID,quantity) values(?,?,?)", userID, item.ProductID, item.Quantity)
 	if queryErr != nil {
 		log.Fatal(queryErr)
 	}
+	defer rows.Close()
 }
 func (c *CartRepository) GetCart(userID string) model.Cart {
 	log.Println("user in repository ", userID)
@@ -48,6 +49,7 @@ func (c *CartRepository) GetCart(userID string) model.Cart {
 		items = append(items, item)
 
 	}
+	defer rows.Close()
 	cart.Items = items
 	log.Println("Cart in repository ", cart)
 	return cart
@@ -73,11 +75,12 @@ func (c *CartRepository) AddOrUpdateCartItem(userID string, item model.Item) {
 	if updateQueryErr != nil {
 		log.Fatal(updateQueryErr)
 	}
-
+	defer rows.Close()
 }
 func (c *CartRepository) EmptyCart(userID string) {
-	_, queryErr := c.Db.Query("delete from cart where userID=?", userID)
+	rows, queryErr := c.Db.Query("delete from cart where userID=?", userID)
 	if queryErr != nil {
 		log.Fatal(queryErr)
 	}
+	defer rows.Close()
 }
